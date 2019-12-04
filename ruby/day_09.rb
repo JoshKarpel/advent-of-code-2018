f863451066038ab01a8330adfcb18099e19d27f2
JoshKarpel <josh.karpel@gmail.com>
2019-12-03 20:13:25 -0600

    solve day 09 part 2

--- ruby/day_09.rb
+++ ruby/day_09.rb
@@ -1,20 +1,54 @@
 # frozen_string_literal: true
 
+class Marble
+  attr_reader :score
+  attr_accessor :ccw, :cw
+
+  def initialize(score, ccw, cw)
+    @score = score
+    @ccw = ccw || self
+    @cw = cw || self
+  end
+
+  def move_ccw(n)
+    marble = self
+    n.times do
+      marble = marble.ccw
+    end
+    marble
+  end
+
+  def move_cw(n)
+    marble = self
+    n.times do
+      marble = marble.cw
+    end
+    marble
+  end
+end
+
 def play(num_players, num_marbles)
-  marbles = [0]
+  current_marble = Marble.new(0, nil, nil)
   scores = Hash.new(0)
-  current_marble_idx = 0
   current_player = 0
 
   (1...num_marbles).each do |marble_number|
-    puts 100 * marble_number / num_marbles.to_f if (marble_number % 10000).zero?
-
     if (marble_number % 23).zero?
-      current_marble_idx = (current_marble_idx - 7) % marbles.length
-      scores[current_player] += marble_number + marbles.delete_at(current_marble_idx)
+      remove = current_marble.move_ccw 7
+      before = remove.move_ccw 1
+      after = remove.move_cw 1
+      before.cw = after
+      after.ccw = before
+      current_marble = after
+
+      scores[current_player] += marble_number + remove.score
     else
-      current_marble_idx = (current_marble_idx + 2) % marbles.length
-      marbles.insert(current_marble_idx, marble_number)
+      before = current_marble.move_cw 1
+      after = before.move_cw 1
+
+      current_marble = Marble.new(marble_number, before, after)
+      before.cw = current_marble
+      after.ccw = current_marble
     end
 
     current_player = (current_player + 1) % num_players
